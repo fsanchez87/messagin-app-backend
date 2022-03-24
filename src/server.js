40110b0d37d6a0617cdda0506340dc519a82eede
Francisco S <sanchezgarridof@gmail.com>
2022-03-24 09:33:34 +0100

    Initial Back-End Setup

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+import Cors from 'cors'
+
+//App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = process.env.MONGO_URI;
+
+//Middleware
+app.use(express.json())
+app.use(Cors())
+
+//DB config
+mongoose.connect(connection_url);
+
+//API endpoint
+app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"));
+
+//Listener
+app.listen(port, () => console.log(`Listening on localhost ${port}`));
