9723ae7a5141d2516aa1f81cfdc536a60ed23aa2
Francisco S <sanchezgarridof@gmail.com>
2022-03-25 07:49:01 +0100

    Adding Pusher to the Back End

--- src/server.js
+++ src/server.js
@@ -3,11 +3,19 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import Cors from "cors";
 import Messages from "./dbMessages.js";
+import Pusher from "pusher";
 
 //App Config
 const app = express();
 const port = process.env.PORT || 8001;
 const connection_url = process.env.MONGO_URI;
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: "eu",
+  useTLS: true,
+});
 
 //Middleware
 app.use(express.json());
@@ -17,6 +25,27 @@ app.use(Cors());
 mongoose.connect(connection_url);
 
 //API endpoint
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB Connected");
+  const msgCollection = db.collection("messagingmessages");
+  const changeStream = msgCollection.watch();
+  changeStream.on("change", (change) => {
+    console.log(change);
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error trigerring Pusher");
+    }
+  });
+});
+
 app.get("/", (req, res) => res.status(200).send("Hello TheWebDev"));
 
 app.post("/messages/new", (req, res) => {
